e7658a36e18057aba73cd69f0054484006ab1f4c
David Drysdale <drysdale@google.com>
2023-12-20 16:41:13 +0000

    rdroidtest: add support for parameterized tests
    Test: librdroidtest_test
    Change-Id: I1b473a810181598ef9d94bc5ff4eb38ffa9744a3

--- libraries/rdroidtest/src/runner.rs
+++ libraries/rdroidtest/src/runner.rs
@@ -9,16 +9,27 @@ use std::env;
 /// Command-line arguments to ignore, because they are not supported by libtest-mimic.
 const IGNORED_ARGS: [&str; 2] = ["-Zunstable-options", "--report-time"];
 
-/// The collection of all tests to run.
+/// The collection of all non-parameterized tests to run.
 #[doc(hidden)]
 #[distributed_slice]
 pub static RDROIDTEST_TESTS: [fn() -> Trial] = [..];
+
+/// The collection of all parameterized tests to run.
+#[doc(hidden)]
+#[distributed_slice]
+pub static RDROIDTEST_PTESTS: [fn() -> Vec<Trial>] = [..];
 
 /// Runs all tests.
 pub fn main() {
     logger::init(logger::Config::default().with_min_level(Level::Debug));
     let args = Arguments::from_iter(env::args().filter(|arg| !IGNORED_ARGS.contains(&arg.deref())));
-    let tests = RDROIDTEST_TESTS.iter().map(|test| test()).collect();
+
+    let tests = RDROIDTEST_TESTS
+        .iter()
+        .map(|test| test())
+        .chain(RDROIDTEST_PTESTS.iter().flat_map(|test| test()))
+        .collect();
+
     libtest_mimic::run(&args, tests).exit();
 }
 
