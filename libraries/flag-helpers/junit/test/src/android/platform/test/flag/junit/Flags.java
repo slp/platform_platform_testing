a0317f75d3dfe2a18de67cff15614aede1e06a43
Liana Kazanova <lkazanova@google.com>
2024-05-03 18:24:34 +0000

    Revert "find the first flag class"
    This reverts commit 850c97f5800e97abf8fa0819a5e2cad76fb40394.
    
    Reason for revert: DroidMonitor. Potential culprit for b/338598783 - verifying through ABTD before submission. 
    
    Change-Id: Id72ff4401d2f31aa33ebb0346f745dcc641bdf7e

--- libraries/flag-helpers/junit/test/src/android/platform/test/flag/junit/Flags.java
+++ libraries/flag-helpers/junit/test/src/android/platform/test/flag/junit/Flags.java
@@ -25,16 +25,6 @@ public class Flags {
     public static final String FLAG_RO_DISABLED = "android.platform.test.flag.junit.ro_disabled";
 
     /** Returns the flag value. */
-    public static boolean flagName1() {
-        return true;
-    }
-
-    /** Returns a non-boolean flag value. */
-    public static int flagName2() {
-        return 1;
-    }
-
-    /** Returns the flag value. */
     public static boolean flagName3() {
         return FEATURE_FLAGS.flagName3();
     }
