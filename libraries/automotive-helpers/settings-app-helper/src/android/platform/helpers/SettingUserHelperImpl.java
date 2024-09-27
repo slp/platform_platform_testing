c0bb1c8cfc4dd69d9da97a61dc6e848b6a33aa27
Rajesh Bayyavarapu <rbayyavarapu@google.com>
2024-09-27 13:13:19 +0000

    Updated test methods in MultiUserTests
    Test: https://android-build.corp.google.com/builds/abtd/run/L41800030006768350
    Bug: 368981109
    Change-Id: Ib60e633fd73b806872490f5dd9f8af7dfd77d5c4

--- libraries/automotive-helpers/settings-app-helper/src/android/platform/helpers/SettingUserHelperImpl.java
+++ libraries/automotive-helpers/settings-app-helper/src/android/platform/helpers/SettingUserHelperImpl.java
@@ -88,7 +88,7 @@ public class SettingUserHelperImpl extends AbstractStandardAppHelper implements 
     public void addUser() {
         clickbutton(AutomotiveConfigConstants.USER_SETTINGS_ADD_PROFILE);
         clickbutton(AutomotiveConfigConstants.USER_SETTINGS_OK);
-        getSpectatioUiUtil().wait5Seconds();
+        getSpectatioUiUtil().waitNSeconds(WAIT_SEC);
     }
     // opens permission page of a user
     @Override
@@ -164,7 +164,6 @@ public class SettingUserHelperImpl extends AbstractStandardAppHelper implements 
     @Override
     public void switchUser(String userFrom, String userTo) {
         int count = 0;
-        goToQuickSettings();
         BySelector userFromSelector = By.text(userFrom);
         UiObject2 userFromObject = getSpectatioUiUtil().findUiObject(userFromSelector);
         getSpectatioUiUtil().validateUiObject(userFromObject, String.format("User %s", userFrom));
@@ -188,9 +187,6 @@ public class SettingUserHelperImpl extends AbstractStandardAppHelper implements 
     @Override
     public void addUserQuickSettings(String userFrom) {
         goToQuickSettings();
-        BySelector userFromSelector = By.text(userFrom);
-        UiObject2 userFromObject = getSpectatioUiUtil().findUiObject(userFromSelector);
-        getSpectatioUiUtil().validateUiObject(userFromObject, String.format("user %s", userFrom));
         getSpectatioUiUtil().wait1Second();
         addUser();
     }
@@ -253,7 +249,7 @@ public class SettingUserHelperImpl extends AbstractStandardAppHelper implements 
 
     // go to quick Settings for switching User
     private void goToQuickSettings() {
-        clickbutton(AutomotiveConfigConstants.USER_SETTINGS_MAKE_TIME_PATTERN);
+        clickbutton(AutomotiveConfigConstants.HOME_PROFILE_ICON_BUTTON);
     }
     // checks whether the Add profile button is visible
     @Override
