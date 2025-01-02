b1e28b7beb6d41a4d385da6f459ba8052bbcd4fa
Zhi Dou <zhidou@google.com>
2025-01-02 16:21:13 +0000

    Add AconfigPerfTests to build artifacts
    AconfigPerfTests is the apk includes all crystalball tests for aconfig
    flags
    
    Test: presubmit
    Bug: 383740537
    Change-Id: I2f8b2d9a4da50db4519f657a50fc1678e4b5ef60

--- build/tasks/tests/instrumentation_metric_test_list.mk
+++ build/tasks/tests/instrumentation_metric_test_list.mk
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 instrumentation_metric_tests := \
+    AconfigPerfTests \
     ActivityManagerPerfTests \
     ActivityManagerPerfTestsTestApp \
     ActivityManagerPerfTestsStubApp1 \
