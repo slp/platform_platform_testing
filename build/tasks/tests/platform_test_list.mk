91590bdca510a465b347ef3e810d04d658c994cb
Julien Desprez <jdesprez@google.com>
2025-01-06 13:53:09 -0800

    Remove unused target from platform_tests build
    It's not useful to build those things
    
    Test: presubmit
    Bug: 342645560
    Change-Id: Ie13d51dd24e0d4bdd7e933d8a8704a092304174d

--- build/tasks/tests/platform_test_list.mk
+++ build/tasks/tests/platform_test_list.mk
@@ -11,9 +11,6 @@ platform_tests += \
     AppCompatibilityTest \
     AppLaunch \
     AppTransitionTests \
-    AutoLocTestApp \
-    AutoLocVersionedTestApp_v1 \
-    AutoLocVersionedTestApp_v2 \
     BackgroundDexOptServiceIntegrationTests \
     BandwidthEnforcementTest \
     BandwidthTests \
@@ -27,22 +24,12 @@ platform_tests += \
     ConnectivityManagerTest \
     ContactsTests \
     CtsCameraTestCases \
-    CtsHardwareTestCases \
-    DataIdleTest \
     Development \
     DeviceHealthChecks \
     DynamicCodeLoggerIntegrationTests \
     DialerJankTests \
     DownloadManagerTestApp \
     StubIME \
-    ExternalLocAllPermsTestApp \
-    ExternalLocTestApp \
-    ExternalLocVersionedTestApp_v1 \
-    ExternalLocVersionedTestApp_v2 \
-    ExternalSharedPermsBTTestApp \
-    ExternalSharedPermsDiffKeyTestApp \
-    ExternalSharedPermsFLTestApp \
-    ExternalSharedPermsTestApp \
     flatland \
     FrameworkPerf \
     FrameworkPermissionTests \
@@ -56,16 +43,6 @@ platform_tests += \
     long_trace_binder_config.textproto \
     long_trace_config.textproto \
     MemoryUsage \
-    MultiDexLegacyTestApp \
-    MultiDexLegacyTestApp2 \
-    MultiDexLegacyTestServices \
-    MultiDexLegacyTestServicesTests \
-    MultiDexLegacyVersionedTestApp_v1 \
-    MultiDexLegacyVersionedTestApp_v2 \
-    MultiDexLegacyVersionedTestApp_v3 \
-    NoLocTestApp \
-    NoLocVersionedTestApp_v1 \
-    NoLocVersionedTestApp_v2 \
     OverviewFunctionalTests \
     perfetto_trace_processor_shell \
     PerformanceAppTest \
@@ -99,16 +76,7 @@ platform_tests += \
     UiBenchJankTests \
     UiBenchJankTestsWear \
     UiBenchMicrobenchmark \
-    UpdateExternalLocTestApp_v1_ext \
-    UpdateExternalLocTestApp_v2_none \
-    UpdateExtToIntLocTestApp_v1_ext \
-    UpdateExtToIntLocTestApp_v2_int \
     uwb_snippet \
-    VersatileTestApp_Auto \
-    VersatileTestApp_External \
-    VersatileTestApp_Internal \
-    VersatileTestApp_None \
-    VoiceInteraction \
     WifiStrengthScannerUtil \
 
 ifneq ($(strip $(BOARD_PERFSETUP_SCRIPT)),)
